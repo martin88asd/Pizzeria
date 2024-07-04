4ed4ee84fc3f486bc57cac147aa6d98115b92471
martin88asd <106070836+martin88asd@users.noreply.github.com>
2024-07-04 10:53:58 +0200

    Add project files.

---
+++ Pizzeria/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Pizzeria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKicsi = new System.Windows.Forms.Label();
+            this.lblNagy = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.rdBtn1Kicsi = new System.Windows.Forms.RadioButton();
+            this.rdBtn1Nagy = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDb1 = new System.Windows.Forms.TextBox();
+            this.txtDb2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rdBtn2Kicsi = new System.Windows.Forms.RadioButton();
+            this.rdBtn2Nagy = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSzamol = new System.Windows.Forms.Button();
+            this.btnTorol = new System.Windows.Forms.Button();
+            this.btnAdatBe = new System.Windows.Forms.Button();
+            this.btnBezar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFizetendo = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblKicsi
+            // 
+            this.lblKicsi.AutoSize = true;
+            this.lblKicsi.Location = new System.Drawing.Point(248, 35);
+            this.lblKicsi.Name = "lblKicsi";
+            this.lblKicsi.Size = new System.Drawing.Size(36, 13);
+            this.lblKicsi.TabIndex = 0;
+            this.lblKicsi.Text = "32 cm";
+            // 
+            // lblNagy
+            // 
+            this.lblNagy.AutoSize = true;
+            this.lblNagy.Location = new System.Drawing.Point(382, 35);
+            this.lblNagy.Name = "lblNagy";
+            this.lblNagy.Size = new System.Drawing.Size(36, 13);
+            this.lblNagy.TabIndex = 1;
+            this.lblNagy.Text = "45 cm";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(75, 106);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(88, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Vegetarianus";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(75, 184);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Magyaros";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // rdBtn1Kicsi
+            // 
+            this.rdBtn1Kicsi.AutoSize = true;
+            this.rdBtn1Kicsi.Location = new System.Drawing.Point(15, 19);
+            this.rdBtn1Kicsi.Name = "rdBtn1Kicsi";
+            this.rdBtn1Kicsi.Size = new System.Drawing.Size(55, 17);
+            this.rdBtn1Kicsi.TabIndex = 4;
+            this.rdBtn1Kicsi.TabStop = true;
+            this.rdBtn1Kicsi.Text = "890 Ft";
+            this.rdBtn1Kicsi.UseVisualStyleBackColor = true;
+            // 
+            // rdBtn1Nagy
+            // 
+            this.rdBtn1Nagy.AutoSize = true;
+            this.rdBtn1Nagy.Location = new System.Drawing.Point(140, 19);
+            this.rdBtn1Nagy.Name = "rdBtn1Nagy";
+            this.rdBtn1Nagy.Size = new System.Drawing.Size(61, 17);
+            this.rdBtn1Nagy.TabIndex = 5;
+            this.rdBtn1Nagy.TabStop = true;
+            this.rdBtn1Nagy.Text = "1290 Ft";
+            this.rdBtn1Nagy.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdBtn1Kicsi);
+            this.groupBox1.Controls.Add(this.rdBtn1Nagy);
+            this.groupBox1.Location = new System.Drawing.Point(233, 96);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 42);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Válasszon";
+            // 
+            // txtDb1
+            // 
+            this.txtDb1.Location = new System.Drawing.Point(505, 106);
+            this.txtDb1.Name = "txtDb1";
+            this.txtDb1.Size = new System.Drawing.Size(36, 20);
+            this.txtDb1.TabIndex = 6;
+            // 
+            // txtDb2
+            // 
+            this.txtDb2.Location = new System.Drawing.Point(505, 181);
+            this.txtDb2.Name = "txtDb2";
+            this.txtDb2.Size = new System.Drawing.Size(36, 20);
+            this.txtDb2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(547, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "darab";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(547, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "darab";
+            // 
+            // rdBtn2Kicsi
+            // 
+            this.rdBtn2Kicsi.AutoSize = true;
+            this.rdBtn2Kicsi.Location = new System.Drawing.Point(3, 13);
+            this.rdBtn2Kicsi.Name = "rdBtn2Kicsi";
+            this.rdBtn2Kicsi.Size = new System.Drawing.Size(55, 17);
+            this.rdBtn2Kicsi.TabIndex = 5;
+            this.rdBtn2Kicsi.TabStop = true;
+            this.rdBtn2Kicsi.Text = "950 Ft";
+            this.rdBtn2Kicsi.UseVisualStyleBackColor = true;
+            // 
+            // rdBtn2Nagy
+            // 
+            this.rdBtn2Nagy.AutoSize = true;
+            this.rdBtn2Nagy.Location = new System.Drawing.Point(135, 13);
+            this.rdBtn2Nagy.Name = "rdBtn2Nagy";
+            this.rdBtn2Nagy.Size = new System.Drawing.Size(61, 17);
+            this.rdBtn2Nagy.TabIndex = 11;
+            this.rdBtn2Nagy.TabStop = true;
+            this.rdBtn2Nagy.Text = "1410 Ft";
+            this.rdBtn2Nagy.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rdBtn2Nagy);
+            this.panel1.Controls.Add(this.rdBtn2Kicsi);
+            this.panel1.Location = new System.Drawing.Point(238, 181);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(230, 40);
+            this.panel1.TabIndex = 12;
+            // 
+            // btnSzamol
+            // 
+            this.btnSzamol.Location = new System.Drawing.Point(72, 265);
+            this.btnSzamol.Name = "btnSzamol";
+            this.btnSzamol.Size = new System.Drawing.Size(75, 23);
+            this.btnSzamol.TabIndex = 13;
+            this.btnSzamol.Text = "Számol";
+            this.btnSzamol.UseVisualStyleBackColor = true;
+            this.btnSzamol.Click += new System.EventHandler(this.btnSzamol_Click);
+            // 
+            // btnTorol
+            // 
+            this.btnTorol.Location = new System.Drawing.Point(72, 308);
+            this.btnTorol.Name = "btnTorol";
+            this.btnTorol.Size = new System.Drawing.Size(75, 23);
+            this.btnTorol.TabIndex = 14;
+            this.btnTorol.Text = "Töröl";
+            this.btnTorol.UseVisualStyleBackColor = true;
+            this.btnTorol.Click += new System.EventHandler(this.btnTorol_Click);
+            // 
+            // btnAdatBe
+            // 
+            this.btnAdatBe.Location = new System.Drawing.Point(238, 308);
+            this.btnAdatBe.Name = "btnAdatBe";
+            this.btnAdatBe.Size = new System.Drawing.Size(140, 23);
+            this.btnAdatBe.TabIndex = 15;
+            this.btnAdatBe.Text = "Adatbevitel";
+            this.btnAdatBe.UseVisualStyleBackColor = true;
+            this.btnAdatBe.Click += new System.EventHandler(this.btnAdatBe_Click);
+            // 
+            // btnBezar
+            // 
+            this.btnBezar.Location = new System.Drawing.Point(449, 308);
+            this.btnBezar.Name = "btnBezar";
+            this.btnBezar.Size = new System.Drawing.Size(75, 23);
+            this.btnBezar.TabIndex = 16;
+            this.btnBezar.Text = "Bezár";
+            this.btnBezar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.Brown;
+            this.label3.Location = new System.Drawing.Point(189, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Fizetendő:";
+            // 
+            // txtFizetendo
+            // 
+            this.txtFizetendo.Location = new System.Drawing.Point(289, 265);
+            this.txtFizetendo.Name = "txtFizetendo";
+            this.txtFizetendo.Size = new System.Drawing.Size(100, 20);
+            this.txtFizetendo.TabIndex = 18;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 354);
+            this.Controls.Add(this.txtFizetendo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnBezar);
+            this.Controls.Add(this.btnAdatBe);
+            this.Controls.Add(this.btnTorol);
+            this.Controls.Add(this.btnSzamol);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDb2);
+            this.Controls.Add(this.txtDb1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lblNagy);
+            this.Controls.Add(this.lblKicsi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKicsi;
+        private System.Windows.Forms.Label lblNagy;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.RadioButton rdBtn1Kicsi;
+        private System.Windows.Forms.RadioButton rdBtn1Nagy;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtDb1;
+        private System.Windows.Forms.TextBox txtDb2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rdBtn2Kicsi;
+        private System.Windows.Forms.RadioButton rdBtn2Nagy;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSzamol;
+        private System.Windows.Forms.Button btnTorol;
+        private System.Windows.Forms.Button btnAdatBe;
+        private System.Windows.Forms.Button btnBezar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFizetendo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
